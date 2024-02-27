9fcc98d115f622bad384fbcd3b732bed6f5c798a
Amy <134191817+55555-Jyeon@users.noreply.github.com>
2024-02-27 23:12:24 +0900

    [ complete ] : apply intersection-observer

--- mobigram/src/page/home/index.js
+++ mobigram/src/page/home/index.js
@@ -4,38 +4,39 @@ import { FlexCenter } from "../../styles/common.style";
 import { useInfiniteQuery } from "react-query";
 import { Api } from "../../api";
 import { POST_QUERY_KEY } from "../../constants/query-key";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
 
 const HomePage = () => {
-  // Queries
-  const { data: postList, fetchNextPage } = useInfiniteQuery({
+  const containerRef = useRef();
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  const {
+    data: postList,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
     queryKey: [POST_QUERY_KEY.POST_LIST],
     queryFn: ({ pageParam = 1 }) => Api.fetchPostList(pageParam),
     getNextPageParam: (lastPage) => {
       const page = lastPage.page;
-      if (lastPage.total_pages === page) return false;
+      if (lastPage.total_pages === page) return true;
       return page + 1;
     },
   });
 
-  // 스크롤 최하단 시 fetchNextPage실행
-  const handleScroll = () => {
-    const scrollHeight = document.documentElement.scrollHeight;
-    const scrollTop = document.documentElement.scrollTop;
-    const clientHeight = document.documentElement.clientHeight;
-    if (scrollTop + clientHeight >= scrollHeight) return fetchNextPage();
-  };
-
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  });
+    if (inView && hasNextPage) {
+      fetchNextPage();
+      console.log("fetchNextPage called!");
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   return (
     postList && (
-      <Container>
+      <Container ref={containerRef}>
         <Contents>
           {postList?.pages.map((page) => {
             return page?.map((post) =>
@@ -43,6 +44,7 @@ const HomePage = () => {
             );
           })}
         </Contents>
+        <div ref={ref} />
       </Container>
     )
   );
@@ -53,6 +55,7 @@ export default HomePage;
 const Container = styled.div`
   width: 100%;
 `;
+
 const Contents = styled.div`
   width: 100%;
   ${FlexCenter}
--- mobigram/src/router/router.js
+++ mobigram/src/router/router.js
@@ -2,8 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/layout";
 import SignInPage from "../page/sign";
 import SignUpPage from "../page/sign/components/sign-up";
-import ProtectedLayout from "../layout/member-layout";
+import MainLayout from "../layout/main-layout";
 import HomePage from "../page/home";
+import FullLayout from "../layout/full-height-layout";
 import MyPage from "../page/my";
 import ChattingPage from "../page/message";
 import ExplorePage from "../page/explore";
@@ -17,10 +18,15 @@ const router = createBrowserRouter([
     ],
   },
   {
-    element: <ProtectedLayout />,
+    element: <MainLayout />,
     children: [
       { path: "/home", element: <HomePage /> },
       { path: "/home:page", element: <HomePage /> },
+    ],
+  },
+  {
+    element: <FullLayout />,
+    children: [
       { path: "/explore", element: <ExplorePage /> },
       { path: "/my-page", element: <MyPage /> },
       { path: "/message", element: <ChattingPage /> },
